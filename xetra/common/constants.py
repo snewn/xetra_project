bf623e2358285820c8a54d7953561f8e8a663484
Shannon Newn <shannonnewn@Shannons-MacBook-Pro.local>
2022-11-20 17:28:36 +0800

    Entrypoint + pipenv implemented

--- xetra/common/constants.py
+++ xetra/common/constants.py
@@ -1,25 +1,21 @@
 """
 File to store constants
 """
-
 from enum import Enum
-
 
 class S3FileTypes(Enum):
     """
-    Support file types for S3BucketConnector
+    supported file types for S3BucketConnector
     """
-
     CSV = 'csv'
     PARQUET = 'parquet'
 
 
 class MetaProcessFormat(Enum):
     """
-    Formation for MetaProcess class
+    formation for MetaProcess class
     """
-
-    META_FATE_FORMAT = '%Y-%m-%d'
+    META_DATE_FORMAT = '%Y-%m-%d'
     META_PROCESS_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
     META_SOURCE_DATE_COL = 'source_date'
     META_PROCESS_COL = 'datetime_of_processing'
